3bf236a529f3b3a132d10ccbe5e8a543415b70eb
EmersonYe <emersonye@gmail.com>
2019-03-08 12:18:35 -0800

    Add spaces around operators

--- devito.js
+++ devito.js
@@ -12,7 +12,7 @@ var frankNo = 0;
 // Dynamic canvas size
 window.addEventListener("resize", () => {
   // Threshold for resizing to improve performance
-  if(Math.abs(canvas.height + canvas.width - (window.innerHeight + window.innerWidth)) > 10) {
+  if(Math.abs(canvas.height + canvas.width - (window.innerHeight + window.innerWidth)) > 20) {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
   }
@@ -25,16 +25,15 @@ function loadImages(directory) {
   for (i = 1; i <= 32; i++) {
     var img = new Image();
     // Assumes image names are just a number with .jpg extension
-    img.src = directory+i+".jpg";
-    franks[i-1] = img;
+    img.src = directory + i + ".jpg";
+    franks[i - 1] = img;
   }
   return franks;
 }
 
 function drawFrank(imgArray) {
   var img = imgArray[frankNo];
-  ctx.drawImage(img, x, y, img.width/img.height*scale*canvas.width, scale*canvas.width);
-  // ctx.drawImage(img, x, y, img.width*scale, img.height*scale);
+  ctx.drawImage(img, x, y, img.width / img.height * scale * canvas.width, scale * canvas.width);
 }
 
 function draw() {
@@ -46,39 +45,39 @@ function draw() {
   y += dy;
   horzOverlap = 0.75;
   vertOverlap = 0.9;
-  imgWidth = imageArray[0].width/imageArray[0].height*scale*canvas.width;
-  imgHeight = scale*canvas.width;
+  imgWidth = imageArray[0].width / imageArray[0].height * scale * canvas.width;
+  imgHeight = scale * canvas.width;
   
   // Right canvas edge collision
   if(x + dx + imgWidth * horzOverlap > canvas.width) {
     dx = -1 * Math.abs(dx);
     frankNo++;
-    if(frankNo>=32) {
-      frankNo=0;
+    if(frankNo >= 32) {
+      frankNo = 0;
     }
   }
   // Left canvas edge collision
   else if(x + dx + imgWidth * (1-horzOverlap) < 0){
     dx = Math.abs(dx);
     frankNo++;
-    if(frankNo>=32) {
-      frankNo=0;
+    if(frankNo >= 32) {
+      frankNo = 0;
     }
   }
   // Bottom canvas edge collision
   if(y + dy + imgHeight * vertOverlap > canvas.height) {
     dy = -1 * Math.abs(dy);
     frankNo++;
-    if(frankNo>=32) {
-      frankNo=0;
+    if(frankNo >= 32) {
+      frankNo = 0;
     }
   }
   // Top canvas edge collision
-  else if(y + dy + imgHeight * (1-vertOverlap)< 0) {
+  else if(y + dy + imgHeight * (1-vertOverlap) < 0) {
     dy = Math.abs(dy);
     frankNo++;
-    if(frankNo>=32) {
-      frankNo=0;
+    if(frankNo >= 32) {
+      frankNo = 0;
     }
 
   }
